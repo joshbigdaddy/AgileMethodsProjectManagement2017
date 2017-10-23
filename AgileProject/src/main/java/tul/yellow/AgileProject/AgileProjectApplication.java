67b7f0a2a61b5dbc93c742f5d58b81d9a7e3b8ea
Aneta Andrzejewska <aneta.andrzejewska@mobilems.pl>
2017-10-23 15:47:14 +0200

    Init commit

---
+++ AgileProject/src/main/java/tul/yellow/AgileProject/AgileProjectApplication.java
@@ -0,0 +1,13 @@
+package tul.yellow.AgileProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("It works!");
+		SpringApplication.run(AgileProjectApplication.class, args);
+	}
+}
